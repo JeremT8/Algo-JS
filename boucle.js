fb66b9edc23dfeb8a4d618598620ef64abf6ae7e
JeremT8 <jeremytilletpro@gmail.com>
2021-11-26 14:30:12 +0100

    Code upgrade

--- boucle.js
+++ boucle.js
@@ -3,7 +3,7 @@ for (let i = 1; i < 6; i++) {
     star += "*"
     console.log(star)
 }
-*/ 
+
 
 let msg = prompt("Entrez un texte");
 let isNumber = true;
@@ -16,3 +16,21 @@ for (let i = 0; i < msg.length; i++) {
     }   
 }
 console.log(isNumber);
+
+*/ 
+
+let sum = 0,
+    n;
+
+    while (true) {
+        n = parseInt(prompt("Saisissez un nombre entier"));
+        if (n) {
+            sum += n;
+        }
+        else if (confirm("Souhaitez vous arreter ?")) {
+            break;
+        }
+    }
+     
+    alert("La somme est " + sum + " .")
+
